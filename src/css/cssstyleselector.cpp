6bd8d8a380bd972ae5de8e8c0edf79900a02fb95
Andrea Iacovitti <aiacovitti@libero.it>
2014-01-09 21:58:25 +0100

    Code cleanup.
    (Forward-port kdelibs commit f93bc9e2b6751d04923279c04cc57283ad916953)

--- src/css/cssstyleselector.cpp
+++ src/css/cssstyleselector.cpp
@@ -784,7 +784,6 @@ RenderStyle *CSSStyleSelector::styleForElement(ElementImpl *e, RenderStyle *fall
         fontDirty = false;
 
         if (propsToApply.size()) {
-            CSSStyleSelector::style = style;
             for (unsigned int i = 0; i < propsToApply.size(); ++i) {
                 if (fontDirty && propsToApply[i]->priority >= (1 << 30)) {
                     // we are past the font properties, time to update to the
@@ -792,7 +791,7 @@ RenderStyle *CSSStyleSelector::styleForElement(ElementImpl *e, RenderStyle *fall
 #ifdef APPLE_CHANGES
                     checkForGenericFamilyChange(style, parentStyle);
 #endif
-                    CSSStyleSelector::style->htmlFont().update(logicalDpiY);
+                    style->htmlFont().update(logicalDpiY);
                     fontDirty = false;
                 }
                 DOM::CSSProperty *prop = propsToApply[i]->prop;
@@ -804,7 +803,7 @@ RenderStyle *CSSStyleSelector::styleForElement(ElementImpl *e, RenderStyle *fall
 #ifdef APPLE_CHANGES
                 checkForGenericFamilyChange(style, parentStyle);
 #endif
-                CSSStyleSelector::style->htmlFont().update(logicalDpiY);
+                style->htmlFont().update(logicalDpiY);
             }
         }
 
@@ -4290,7 +4289,7 @@ void CSSStyleSelector::applyRule(int id, DOM::CSSValueImpl *value)
             // before we evaluate line-height, e.g., font: 1em/1em.  FIXME: Still not
             // good enough: style="font:1em/1em; font-size:36px" should have a line-height of 36px.
             if (fontDirty) {
-                CSSStyleSelector::style->htmlFont().update(logicalDpiY);
+                style->htmlFont().update(logicalDpiY);
             }
 
             applyRule(CSS_PROP_LINE_HEIGHT, font->lineHeight);
