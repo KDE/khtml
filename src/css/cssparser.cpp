1c85155060094d6743f9a3580562ea6e1222ec8a
Michael Pyne <mpyne@kde.org>
2015-12-21 20:22:48 -0500

    css: Fix potential memleak parsing CSS style values.
    Noted by Coverity, CID 257927
    
    Rather than fixing every potential leaking path, I used a scoped smart
    pointer and manually allowed the one legit ownership transfer I noted in
    the original code.

--- src/css/cssparser.cpp
+++ src/css/cssparser.cpp
@@ -30,6 +30,7 @@
 
 #include <QDebug>
 #include <QUrl>
+#include <QScopedPointer>
 
 #include "css_valueimpl.h"
 #include "css_ruleimpl.h"
@@ -1677,7 +1678,8 @@ bool CSSParser::parse4Values(int propId, const int *properties,  bool important)
 // [ <string> | attr(X) | open-quote | close-quote | no-open-quote | no-close-quote ]+ | inherit
 bool CSSParser::parseContent(int propId, bool important)
 {
-    CSSValueListImpl *values = new CSSValueListImpl(CSSValueListImpl::Comma);
+    QScopedPointer<CSSValueListImpl> values(
+            new CSSValueListImpl(CSSValueListImpl::Comma));
 
     bool isValid = true;
     Value *val;
@@ -1748,12 +1750,11 @@ bool CSSParser::parseContent(int propId, bool important)
         valueList->next();
     }
     if (isValid && values->length()) {
-        addProperty(propId, values, important);
+        addProperty(propId, values.take(), important);
         valueList->next();
         return true;
     }
 
-    delete values;  // also frees any content by deref
     return false;
 }
 
