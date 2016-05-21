520695774bf85cfa9c21f06a29c7a6227dfdff75
Andrea Iacovitti <aiacovitti@libero.it>
2016-05-21 22:41:51 +0200

    Do not create new fontFace if there is no valid source.
    (port of kdelibs commit 362c56e)

--- src/css/css_webfont.cpp
+++ src/css/css_webfont.cpp
@@ -563,22 +563,19 @@ void CSSFontSelector::addFontFaceRule(const CSSFontFaceRuleImpl *fontFaceRule)
             source = new CSSFontFaceSource(item->resource());
         }
 
-        if (!fontFace) {
-            fontFace = new CSSFontFace(static_cast<FontTraitsMask>(traitsMask), this);
-        }
-
         if (source) {
 #if 0
             // ENABLE(SVG_FONTS)
             source->setSVGFontFaceElement(item->svgFontFaceElement());
 #endif
+            if (!fontFace) {
+                fontFace = new CSSFontFace(static_cast<FontTraitsMask>(traitsMask), this);
+            }
             fontFace->addSource(source);
         }
     }
 
-    assert(fontFace);
-
-    if (fontFace && !fontFace->isValid()) {
+    if (!fontFace || !fontFace->isValid()) {
         delete fontFace;
         return;
     }
