2e50f6bb9d3950c726db0e25a5d72cc08cbef447
Friedrich W. H. Kossebau <kossebau@kde.org>
2019-01-10 06:50:23 +0100

    Add JSON metadata to khtmlpart plugin binary

--- src/khtml_factory.h
+++ src/khtml_factory.h
@@ -28,7 +28,7 @@
 class KHTMLFactory : public KPluginFactory
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.kde.KPluginFactory")
+    Q_PLUGIN_METADATA(IID "org.kde.KPluginFactory" FILE "khtml.json")
     Q_INTERFACES(KPluginFactory)
 public:
     KHTMLFactory();
